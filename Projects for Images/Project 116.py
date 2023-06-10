963256fae02c4abaddf167f259f445321752bcfd
BhaktiShri-9 <105867065+BhaktiShri-9@users.noreply.github.com>
2023-06-10 15:22:45 +0530

    Add files via upload

---
+++ Projects for Images/Project 116.py
@@ -0,0 +1,68 @@
+import cv2
+img = cv2.imread('solar-system.jpg')
+cv2.putText(img,
+            'Sun',
+            (20,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Mercury',
+            (50,350),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Venus',
+            (70,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Earth',
+            (100,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Mars',
+            (130,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Jupiter',
+            (170,350),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Saturn',
+            (200,350),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Uranus',
+            (230,350),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.putText(img,
+            'Neptune',
+            (270,350),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (0,0,225)
+            )
+cv2.imwrite('Solar_systemwithname.jpg',img)
+cv2.imshow('output',img)
+cv2.waitKey(0)
